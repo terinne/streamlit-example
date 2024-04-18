f821d0656c7c33c03443cca6c1162d9bdf776d01
Teemu Rinne <101931274+terinne@users.noreply.github.com>
2024-04-18 13:08:39 +0000

    latest version

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,37 @@ import streamlit as st
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report
+
+def analyze():
+    container_1.write(f'Text to analyze: {st.session_state.user_input}')
+    model = st.session_state.model
+    st.session_state.result = model.predict([st.session_state.user_input])[0]
+
+    current_result = st.session_state.result
+
+    if current_result == "positive":
+        container_2.write(f'The result is {st.session_state.result}! :smile:')
+    elif current_result == "neutral":
+        container_2.write(f'The result is {st.session_state.result}! :neutral_face:')
+    elif current_result == "negative":
+        container_2.write(f'The result is {st.session_state.result}! :rage:')
+    
+    st.session_state.user_input = ""
+
+st.title('Sentiment Analysis App')
+st.divider()
+container_3 = st.container(border=True)
+
+st.subheader('Insert your text, press submit and see the results!')
+
+st.text_input('Text', key='user_input',  label_visibility='collapsed', placeholder='Write something...')
+
+st.button('SUBMIT', use_container_width=True, on_click=analyze)
+
+container_1 = st.container(border=True)
+container_2 = st.container(border=True)
+
 
 # read the data and clean it
 df_train = pd.read_csv("train.csv", encoding='unicode_escape')
@@ -21,37 +52,17 @@ x_test = df_test['text']
 y_train = df_train['sentiment']
 y_test = df_test['sentiment']
 
-model = Pipeline([
+st.session_state.model = Pipeline([
     ('tfidf', TfidfVectorizer()),
     ('clf', LinearSVC())
 ])
 
-model.fit(x_train, y_train)
+st.session_state.model.fit(x_train, y_train)
 
-st.title('Sentiment Analysis App')
-st.divider()
-st.subheader('Insert your text, press submit and see the results!')
-
-user_input = st.text_input('Text', label_visibility='collapsed', placeholder='Write something...')
-
-def analyze():
-    global user_input
-    container_1.write(f'Text to analyze: {user_input}')
-    user_input = ""
-
-    result = model.predict([user_input])[0]
-
-    if result == "positive":
-        container_2.write(f'The result is {result}! :smile:')
-    elif result == "neutral":
-        container_2.write(f'The result is {result}! :neutral_face:')
-    elif result == "negative":
-        container_2.write(f'The result is {result}! :rage:')
-
-st.button('SUBMIT', use_container_width=True, on_click=analyze)
-
-container_1 = st.container(border=True)
-container_2 = st.container(border=True)
-
+predictions = st.session_state.model.predict(x_test)
+report = classification_report(y_test, predictions, output_dict=True)
+container_3.write('The results of the training:')
+container_3.dataframe(report)
     
+
 
